aa67a226c92d739c06372b02622d57ea10167d23
ilknur yilmaz <ilknuray1318@gmail.com>
2023-03-11 21:16:28 +0100

    U1

--- src/test/java/tests/C19_BaseUrlKullanimi.java
+++ src/test/java/tests/C19_BaseUrlKullanimi.java
@@ -1,6 +1,11 @@
 package tests;
 
 import baseUrl.RestFullHerokuBaseUrl;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+
+import static io.restassured.RestAssured.given;
 
 public class C19_BaseUrlKullanimi extends RestFullHerokuBaseUrl {
     /*
@@ -8,4 +13,17 @@ public class C19_BaseUrlKullanimi extends RestFullHerokuBaseUrl {
 donen response’un status code’unun 200 oldugunu ve Response’ta 12 booking oldugunu test
 edin
      */
+    public void test(){
+        //set the url
+        specHeroku.pathParam("pp1","booking");
+        //set the expected data
+        //send request and get the response
+
+        Response response=given().spec(specHeroku).when().get("/{pp1}");
+
+        //do assertion
+        JsonPath jsonPath=response.jsonPath();
+        System.out.println(jsonPath.getList("bookingid").size());
+        response.then().assertThat().statusCode(200).body("bookingid", Matchers.hasSize(2000));
+    }
 }
